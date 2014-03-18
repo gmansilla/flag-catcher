17aa0751bf9f092d8dd0e88b7422bfc3df81ef58
Guillermo Mansilla <mansillag@gmail.com>
2014-03-18 16:40:43 -0400

    generating game object

--- routes/sockets.js
+++ routes/sockets.js
@@ -4,7 +4,7 @@ var db = require('../models');
 var cookie  =   require('cookie');
 var connect =   require('connect');
 var userReader = require('../utils/user_reader');
-
+var games = {};
 
 exports.initialize = function (server, sessionStore, cookieParser) {
     io = io.listen(server);
@@ -47,8 +47,14 @@ exports.initialize = function (server, sessionStore, cookieParser) {
                         socket.join(gameId);
                         socket.in(socket.room).broadcast.send(JSON.stringify({message: user.username + ' has joined the game'}));
                         socket.in(socket.room).send(JSON.stringify({message: user.username + ' has joined the game'}));
+                        userJoin(socket);
                     }).error(function(err) {});
             });
+        });
+
+        socket.on('startgame', function() {
+            //check if user calling this is the owner of the game
+
         });
 
     });
@@ -56,4 +62,52 @@ exports.initialize = function (server, sessionStore, cookieParser) {
 
 
 };
+
+function userJoin(socket) {
+    socket.get('user', function(err, user) {
+        if (games[socket.room] == undefined) {
+            user.x = 100;
+            user.y = 200;
+            user.lives = 2;
+            user.energy = 100;
+
+            var flag1 = {
+                x: 0,
+                y: 20,
+                team: 'B',
+                carrier: null
+            }
+
+            var flag2 = {
+                x: 500,
+                y: 970,
+                team: 'A',
+                carrier: null
+            }
+
+            games[socket.room] = {
+                id: socket.room,
+                users: [],
+                mines: [],
+                flag: [flag1, flag2],
+                scoreA: 0,
+                scoreB: 0,
+                timeLeft: 180000
+            }
+
+        } else {
+
+        }
+
+
+
+
+        games[socket.room].users.push(user);
+        console.log('user info is:');
+        util.inspect(console.log(games[socket.room]));
+
+    });
+
+
+}
 
