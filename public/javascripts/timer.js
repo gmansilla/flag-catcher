5b9826ceb70e0d9589e636b43da9eb62c1bb8408
Venompaw <avatar_of_venom@hotmail.com>
2014-04-01 16:07:14 -0400

    Score function not implemented yet

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -4,8 +4,27 @@ function timer() {
 var time = setInterval(function () {
  var timer = document.getElementById("timer").innerHTML = "Timer:" + seconds;
 seconds = seconds-1;
-if(seconds== 0)
-{ var timer = document.getElementById("timer").innerHTML = "Game is over";
+if(seconds== 0&& bluescore== redscore)
+{ seconds = seconds+30;
+
+
 }
+
+else if(seconds == 0)
+{
+
+
+if(bluescore > redscore)
+{
+var timer = document.getElementById("timer").innerHTML = "Blue team wins";
+}
+
+
+if(redscore > bluescore)
+{
+var timer = document.getElementById("timer").innerHTML = "Red team wins";
+}
+}
+
     }, 1000);
 }
